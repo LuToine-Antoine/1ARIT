ef5fb4c26e3960ea07e6fb504630b86a484bb33f
Antoine <antoine.julien@supinfo.com>
2024-04-12 19:51:16 +0200

    dÃebut grid

---
+++ main.py
@@ -0,0 +1,29 @@
+class Grid:
+
+    def __init__(self, lenght):
+        self._grid_lenght = lenght
+        self._grid = []
+
+    def set_lenght(self):
+        self._grid_lenght = int(input("Enter grid lenght: "))
+        print(self._grid_lenght)
+
+    def get_grid_lenght(self):
+        return self._grid_lenght
+
+    def grid_fill(self):
+        print(self.get_grid_lenght())
+        for i in range(self.get_grid_lenght()):
+            print("truc1")
+            for j in range(self._grid_lenght):
+                print("truc2")
+                self._grid.append(0)
+                return self._grid
+
+
+grid = Grid(2)
+
+grid.set_lenght()
+truc = grid.grid_fill()
+print(truc)
+
