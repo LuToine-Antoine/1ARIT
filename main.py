8970b0f8d2a9e9b9909186b622ce6a6596f5db42
☾ Lu <72708201+Lu-LuToine@users.noreply.github.com>
2024-04-14 20:37:58 +0200

    Add adapt_sentence

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 class Grid:
 
-    def __init__(self, lenght):
-        self._grid_lenght = lenght
+    def __init__(self):
+        '''
+        Initialize grid (length, sentence to cipher and grid)
+        '''
+        self._grid_lenght = None
+        self._sentence = None
         self._grid = []
 
     def set_lenght(self):
@@ -9,7 +13,6 @@ class Grid:
         Set the grid lenght
         '''
         self._grid_lenght = int(input("Enter grid lenght: "))
-        print(self._grid_lenght)
 
     def get_grid_lenght(self):
         '''
@@ -17,22 +20,59 @@ class Grid:
         '''
         return self._grid_lenght
 
+    def set_sentence(self):
+        '''
+        Set the sentence
+        '''
+        self._sentence = input("Enter sentence to cipher: ")
+
+    def get_sentence(self):
+        '''
+        Get the sentence
+        '''
+        return self._sentence
+
     def grid_fill(self):
         '''
         Create grid with input lenght, chunk by chunk in empty list
         '''
-        for i in range(1):
-            self._chunk = []
-
-            for j in range(self.get_grid_lenght()):
-                self._chunk.append(0)
-                self._grid.append(self._chunk)
+        self._chunk = []  # Create empty list
+        for i in range(self.get_grid_lenght()):  # Loop to append 0 in chunk, compared to grid lenght
+            self._chunk.append(0)
+            self._grid.append(self._chunk)
 
         return self._grid
 
+    def adapt_sentence(self):
+        '''
+        Adapt sentence with lower, no special caracter and no sapce
+        '''
+        self.set_sentence()
+        replaceCaractere = {"e": "éèêë", "a": "àâä", "u": "ùûü", "i": "îï", "o": "ôö", "c": "ç"}  # Dictionnary of special letters
+        tempsSentence = self.get_sentence().lower()  # Set sentence to lower
+        tempsSentence = tempsSentence.replace(" ", "")  # Remove space
 
-grid = Grid(2)
+        for key, value in replaceCaractere.items():  # Loop in oder to replace caractere (dict. values) in sentence with dict. key
+            for i in value:
+                tempsSentence = tempsSentence.replace(i, key)
+
+        lenOfSentence = len(tempsSentence)-1  # Set sentence lenght
+        while lenOfSentence > 0:  # Loop to remove other sepcial caractere, using not lenght fix because, with remeving, the lenght change
+            if tempsSentence[lenOfSentence] not in "abcdefghijklmnopqrstuvwxyz":
+                tempsSentence = tempsSentence.replace(tempsSentence[lenOfSentence], "")
+            lenOfSentence -= 1
+
+        self._sentence = tempsSentence
+        return self._sentence
+
+
+
+###################################################################
+
+
+grid = Grid()
 
 grid.set_lenght()
 truc = grid.grid_fill()
-print(truc)
+machin = grid.adapt_sentence()
+print(truc, machin)
