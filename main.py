1964ca07d9a198a6c00b0400691ca4e975459e2a
Antoine <antoine.julien@supinfo.com>
2024-04-16 15:20:32 +0200

    ajout des masks

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
 class Grid:
 
     def __init__(self):
-        '''
+        """
         Initialize grid (length, sentence to cipher and grid)
-        '''
+        """
+        self._letters = "abcdefghijklmnopqrstuvwxyz"
         self._grid_lenght = None
         self._sentence = None
         self._grid = []
+        self._mask = []
+
 
     def set_lenght(self):
-        '''
+        """
         Set the grid lenght
-        '''
+        """
         self._grid_lenght = int(input("Enter grid lenght: "))
 
     def get_grid_lenght(self):
-        '''
+        """
         Get the grid lenght
-        '''
+        """
         return self._grid_lenght
 
     def set_sentence(self):
-        '''
+        """
         Set the sentence
-        '''
+        """
         self._sentence = input("Enter sentence to cipher: ")
 
     def get_sentence(self):
-        '''
+        """
         Get the sentence
-        '''
+        """
         return self._sentence
 
     def grid_fill(self):
-        '''
+        """
         Create grid with input lenght, chunk by chunk in empty list
-        '''
-        self._chunk = []  # Create empty list
+        """
+        chunk = []  # Create empty list
         for i in range(self.get_grid_lenght()):  # Loop to append 0 in chunk, compared to grid lenght
-            self._chunk.append(0)
-            self._grid.append(self._chunk)
+            chunk.append(0)
+            self._grid.append(chunk)
 
         return self._grid
 
     def adapt_sentence(self):
-        '''
+        """
         Adapt sentence with lower, no special caracter and no sapce
-        '''
+        """
         self.set_sentence()
-        replaceCaractere = {"e": "éèêë", "a": "àâä", "u": "ùûü", "i": "îï", "o": "ôö", "c": "ç"}  # Dictionnary of special letters
+        replaceCaractere = {"e": "éèêë", "a": "àâä", "u": "ùûü", "i": "îï", "o": "ôö",
+                            "c": "ç"}  # Dictionnary of special letters
         tempsSentence = self.get_sentence().lower()  # Set sentence to lower
         tempsSentence = tempsSentence.replace(" ", "")  # Remove space
 
@@ -56,7 +60,7 @@ class Grid:
             for i in value:
                 tempsSentence = tempsSentence.replace(i, key)
 
-        lenOfSentence = len(tempsSentence)-1  # Set sentence lenght
+        lenOfSentence = len(tempsSentence) - 1  # Set sentence lenght
         while lenOfSentence > 0:  # Loop to remove other sepcial caractere, using not lenght fix because, with remeving, the lenght change
             if tempsSentence[lenOfSentence] not in "abcdefghijklmnopqrstuvwxyz":
                 tempsSentence = tempsSentence.replace(tempsSentence[lenOfSentence], "")
@@ -65,7 +69,22 @@ class Grid:
         self._sentence = tempsSentence
         return self._sentence
 
+    def mask_fill(self):
+        chunkMask = []  # Create empty list
+        for i in range(self.get_grid_lenght()):  # Loop to append 0 in chunk, compared to grid lenght
+            chunkMask.append(0)
+            self._mask.append(chunkMask)
 
+        return self._mask
+
+    def set_mask(self, x, y, mode):
+        if mode == 0:
+            self._mask[x][y] = 0
+        elif mode == 1:
+            self._mask[x][y] = 1
+
+    def get_mask(self):
+        return self._mask
 
 ###################################################################
 
